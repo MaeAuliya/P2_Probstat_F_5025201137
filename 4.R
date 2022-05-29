42772bc37f9f822c810bf22c1f3c2c37766d117f
Maisan Auliya <103273340+MaeAuliya@users.noreply.github.com>
2022-05-29 23:40:10 +0700

    Create 4.R

---
+++ 4.R
@@ -0,0 +1,38 @@
+#No 4A
+#Import File
+myFile  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+dim(myFile)
+head(myFile)
+attach(myFile)
+
+myFile$V1 <- as.factor(myFile$V1)
+myFile$V1 = factor(myFile$V1,labels = c("Kucing Oren","Kucing Hitam","Kucing Putih","Kucing Oren"))
+
+class(myFile$V1)
+
+group1 <- subset(myFile, V1=="Kucing Oren")
+group2 <- subset(myFile, V1=="Kucing Hitam")
+group3 <- subset(myFile, V1=="Kucing Putih")
+
+
+#NO 4B
+bartlett.test(Length~V1, data=dataoneway)
+
+
+#NO 4C
+qqnorm(group1$Length)
+qqline(group1$Length)
+
+
+#NO 4E
+model1 <- lm(Length~Group, data=myFile)
+
+anova(model1)
+
+TukeyHSD(aov(model1))
+
+
+#NO 4F
+library(ggplot2)
+ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + 
+  scale_x_discrete() + xlab("Treatment Group") +  ylab("Length (cm)")
